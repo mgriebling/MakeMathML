5e268fc3e794c34c595eb622cfdd61f4c4e56f10
Michael Griebling <sam@c-inspirations.com>
2017-08-13 04:49:23 -0400

    Initial Commit

---
+++ MakeMathML/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MakeMathML
+//
+//  Created by Mike Griebling on 13 Aug 2017.
+//  Copyright © 2017 Computer Inspirations. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
